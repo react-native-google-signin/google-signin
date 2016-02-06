abd0d72e90b0c006a2215b23ac5b499336327352
devfd <apptailor.co@gmail.com>
2016-02-06 08:56:12 +0100

    match ios and android api

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,139 @@
-var {
-  NativeModules: {
-    RNGoogleSignin
-  }
-} = require('react-native');
 
-module.exports = RNGoogleSignin;
+const React = require('react-native');
+
+const {
+  View,
+  PropTypes,
+  NativeAppEventEmitter,
+  NativeModules: { RNGoogleSignin },
+  requireNativeComponent,
+} = React;
+
+
+const RNGoogleSigninButton = requireNativeComponent('RNGoogleSigninButton', null);
+
+class GoogleSigninButton extends React.Component {
+  componentDidMount() {
+    this._clickListener = NativeAppEventEmitter.addListener('RNGoogleSignInWillDispatch', () => {
+      GoogleSigninSingleton.signinIsInProcess = true;
+      this.props.onPress && this.props.onPress();
+    });
+  }
+
+  componentWillUnmount() {
+    this._clickListener && this._clickListener.remove();
+  }
+
+  render() {
+    return (
+      <RNGoogleSigninButton {...this.props}/>
+    );
+  }
+}
+
+GoogleSigninButton.Size = {
+  Icon: RNGoogleSignin.BUTTON_SIZE_ICON,
+  Standard: RNGoogleSignin.BUTTON_SIZE_STANDARD,
+  Wide: RNGoogleSignin.BUTTON_SIZE_WIDE
+};
+
+GoogleSigninButton.Color = {
+  Auto: RNGoogleSignin.BUTTON_COLOR_AUTO,
+  Light: RNGoogleSignin.BUTTON_COLOR_LIGHT,
+  Dark: RNGoogleSignin.BUTTON_COLOR_DARK
+};
+
+class GoogleSignin {
+
+  constructor() {
+    this._user = null;
+    this.signinIsInProcess = false;
+  }
+
+  configure(params={}) {
+    if (!params.iosClientId) {
+      throw new Error('GoogleSignin - Missing iOS app ClientID');
+    }
+
+    if (params.offlineAccess && !params.webClientId) {
+      throw new Error('GoogleSignin - offline use requires server web ClientID');
+    }
+
+    params = [
+      params.scopes || [], params.iosClientId, params.offlineAccess ? params.webClientId : ''
+    ];
+
+    RNGoogleSignin.configure(...params);
+  }
+
+  currentUserAsync() {
+    return new Promise((resolve, reject) => {
+      const sucessCb = NativeAppEventEmitter.addListener('RNGoogleSignInSuccess', (user) => {
+        this._user = user;
+        this._removeListeners(sucessCb, errorCb);
+        resolve(user);
+      });
+
+      const errorCb = NativeAppEventEmitter.addListener('RNGoogleSignInError', () => {
+        this._removeListeners(sucessCb, errorCb);
+        resolve(null);
+      });
+
+      RNGoogleSignin.currentUserAsync();
+    });
+  }
+
+  currentUser() {
+    return {...this._user};
+  }
+
+  signIn() {
+    return new Promise((resolve, reject) => {
+      const sucessCb = NativeAppEventEmitter.addListener('RNGoogleSignInSuccess', (user) => {
+        this._user = user;
+        this.signinIsInProcess = false;
+        this._removeListeners(sucessCb, errorCb);
+        resolve(user);
+      });
+
+      const errorCb = NativeAppEventEmitter.addListener('RNGoogleSignInError', (err) => {
+        this._removeListeners(sucessCb, errorCb);
+        this.signinIsInProcess = false;
+        reject(err);
+      });
+
+      !this.signinIsInProcess && RNGoogleSignin.signIn();
+    });
+  }
+
+  signOut() {
+    return new Promise((resolve, reject) => {
+      RNGoogleSignin.signOut();
+      resolve();
+    });
+  }
+
+  revokeAccess() {
+    return new Promise((resolve, reject) => {
+      const sucessCb = NativeAppEventEmitter.addListener('RNGoogleRevokeSuccess', () => {
+        this._removeListeners(sucessCb, errorCb);
+        resolve();
+      });
+
+      const errorCb = NativeAppEventEmitter.addListener('RNGoogleRevokeError', (err) => {
+        this._removeListeners(sucessCb, errorCb);
+        reject(err);
+      });
+
+      RNGoogleSignin.revokeAccess();
+    });
+  }
+
+  _removeListeners(...listeners) {
+    listeners.forEach(lt => lt.remove());
+  }
+}
+
+const GoogleSigninSingleton = new GoogleSignin();
+
+module.exports = {GoogleSignin: GoogleSigninSingleton, GoogleSigninButton};
