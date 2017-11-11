135646225989d63b8f2bb5f67c9ff0a551584638
Johan Ruokangas <johan@latehours.net>
2017-11-11 15:07:21 +0200

    Relink packages and install missing peer deps

--- example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
+++ example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
           new RNGoogleSigninPackage()
       );
     }
