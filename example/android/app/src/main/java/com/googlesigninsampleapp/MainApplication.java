5f09f18419539de3ac21b30754ff69571fb1e4d5
Johan Ruokangas <johan@latehours.net>
2017-11-11 15:16:58 +0200

    de integrate everything

--- example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
+++ example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGoogleSigninPackage(),
           new RNGoogleSigninPackage()
       );
     }
