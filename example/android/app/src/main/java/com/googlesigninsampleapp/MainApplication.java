622de5df07c8cb777fac5ac58219de547feb1fd0
Johan Ruokangas <johan@latehours.net>
2018-05-20 11:45:53 +0300

    Upgrade to RN 0.55, use Cocoapods on iOS and fix Android build

--- example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
+++ example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
@@ -27,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
           new RNGoogleSigninPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
