ac45fe22b8e0dacbf2c228627fbceb3670b7007d
Johan Ruokangas <johan@latehours.net>
2018-09-10 09:37:29 +0300

    Reformat

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-export {
-  GoogleSigninSingleton as GoogleSignin,
-  statusCodes,
-} from './src/GoogleSignin';
+export { GoogleSigninSingleton as GoogleSignin, statusCodes } from './src/GoogleSignin';
 
 export { GoogleSigninButton } from './src/GoogleSigninButton';
