64058b8f765d7f737a1def8e3a7e91b2667283cd
Michael Elsdörfer <michael@elsdoerfer.info>
2018-10-22 11:42:43 +0100

    If the native module is unlinked, raise an error on usage.

--- src/GoogleSignin.js
+++ src/GoogleSignin.js
@@ -6,23 +6,28 @@ const { RNGoogleSignin } = NativeModules;
 
 const IS_IOS = Platform.OS === 'ios';
 
+const NativeModuleLoaded = RNGoogleSignin !== undefined;
+
+function ensureNativeNoduleLoaded() {
+  if (!NativeModuleLoaded) {
+    throw new Error(
+      'RN GoogleSignin native module is not correctly linked. Please read the readme, setup and troubleshooting instructions carefully or try manual linking.'
+    );
+  }
+}
+
 class GoogleSignin {
   configPromise;
 
-  constructor() {
-    if (__DEV__ && !RNGoogleSignin) {
-      console.error(
-        'RN GoogleSignin native module is not correctly linked. Please read the readme, setup and troubleshooting instructions carefully or try manual linking.'
-      );
-    }
-  }
-
   async signIn() {
+    ensureNativeModuleLoaded();
     await this.configPromise;
     return await RNGoogleSignin.signIn();
   }
 
   async hasPlayServices(options = { showPlayServicesUpdateDialog: true }) {
+    ensureNativeModuleLoaded();
+
     if (IS_IOS) {
       return true;
     } else {
@@ -36,6 +41,8 @@ class GoogleSignin {
   }
 
   configure(options = {}) {
+    ensureNativeModuleLoaded();
+
     if (options.offlineAccess && !options.webClientId) {
       throw new Error('RNGoogleSignin: offline use requires server web ClientID');
     }
@@ -44,26 +51,34 @@ class GoogleSignin {
   }
 
   async signInSilently() {
+    ensureNativeModuleLoaded();
+
     await this.configPromise;
     return RNGoogleSignin.signInSilently();
   }
 
   async signOut() {
+    ensureNativeModuleLoaded();
+
     return RNGoogleSignin.signOut();
   }
 
   async revokeAccess() {
+    ensureNativeModuleLoaded();
+
     return RNGoogleSignin.revokeAccess();
   }
 
   async isSignedIn() {
+    ensureNativeModuleLoaded();
+    
     return RNGoogleSignin.isSignedIn();
   }
 }
 
 export const GoogleSigninSingleton = new GoogleSignin();
 
-export const statusCodes = {
+export const statusCodes = NativeModuleLoaded ? {
   SIGN_IN_CANCELLED: RNGoogleSignin.SIGN_IN_CANCELLED,
   IN_PROGRESS: RNGoogleSignin.IN_PROGRESS,
   PLAY_SERVICES_NOT_AVAILABLE: RNGoogleSignin.PLAY_SERVICES_NOT_AVAILABLE,
