70db84d3211d3352cc42f98ab8dd200f085c1c67
Vojtech Novak <vonovak@gmail.com>
2018-11-03 16:57:50 -0300

    Update flowTypes.js
    Co-Authored-By: AndreiCalazans <andreixoc@hotmail.com>

--- flowTypes.js
+++ flowTypes.js
@@ -23,7 +23,7 @@ export type User = {
   user: {
     email: string,
     id: string,
-    givenName: string,
+    givenName: ?string,
     familyName: string,
     photo: string, // url
     name: string, // full name
