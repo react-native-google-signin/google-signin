d349916061bbfb77420ca08b4933e02b34d6446e
Trương Anh Quốc <quocta@talent.ominext.com>
2019-06-26 21:35:48 +0700

    Replace "android.support.annotation" to "androidx.annotation"

--- android/src/main/java/co/apptailor/googlesignin/RNGoogleSigninModule.java
+++ android/src/main/java/co/apptailor/googlesignin/RNGoogleSigninModule.java
@@ -4,8 +4,8 @@ import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
