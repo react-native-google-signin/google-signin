943fe655bcdd11ec150a3adb08224c6ab9f5933c
Vojtech Novak <vonovak@gmail.com>
2019-10-01 20:05:50 +0200

    one more comment for future reference

--- example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
+++ example/android/app/src/main/java/com/googlesigninsampleapp/MainApplication.java
@@ -30,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected String getJSMainModuleName() {
+          // edited for example app
           return "example/index";
         }
       };
