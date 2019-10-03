f13692560488db52b313f2b6853c07bf36c0f68d
Vojtech Novak <vonovak@gmail.com>
2019-10-03 08:50:25 +0200

    do not use forbidden chars in RN-config.js

--- react-native.config.js
+++ react-native.config.js
@@ -10,8 +10,8 @@ module.exports = {
   },
   dependencies: {
     // the key here does not matter as long as it's not referenced from native files
-    // CLI will use root to find podspec and android module
-    '@react-native-community/google-signin': {
+    // CLI will use root to find podspec and android module.
+    'react-native-google-signin': {
       root,
     },
   },
