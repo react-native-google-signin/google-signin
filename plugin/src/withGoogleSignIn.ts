8cfb60d09df57e58e78af5f7d47e41889c7c0723
Giau. Tran Minh <giau.tmg@gmail.com>
2022-03-01 00:02:19 +0700

    feat: create expo plugin

---
+++ plugin/src/withGoogleSignIn.ts
@@ -0,0 +1,27 @@
+import {
+  ConfigPlugin,
+  AndroidConfig,
+  IOSConfig,
+  createRunOncePlugin,
+  withPlugins,
+} from '@expo/config-plugins';
+
+const pkg = require('@react-native-google-signin/google-signin/package.json');
+
+/**
+ * Apply google-signin configuration for Expo SDK 44 projects.
+ */
+const withGoogleSignIn: ConfigPlugin = (config) => {
+  return withPlugins(config, [
+    // Android
+    AndroidConfig.GoogleServices.withClassPath,
+    AndroidConfig.GoogleServices.withApplyPlugin,
+    AndroidConfig.GoogleServices.withGoogleServicesFile,
+
+    // iOS
+    IOSConfig.Google.withGoogle,
+    IOSConfig.Google.withGoogleServicesFile,
+  ]);
+};
+
+export default createRunOncePlugin(withGoogleSignIn, pkg.name, pkg.version);
