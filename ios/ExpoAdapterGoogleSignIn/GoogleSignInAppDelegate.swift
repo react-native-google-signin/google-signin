c1c88ea48ae6443d8bb5bcf68bba1c3f21ab3f2b
Giau. Tran Minh <giau.tmg@gmail.com>
2022-03-15 23:59:42 +0700

    fix: conditional import ExpoModulesCore

--- ios/ExpoAdapterGoogleSignIn/GoogleSignInAppDelegate.swift
+++ ios/ExpoAdapterGoogleSignIn/GoogleSignInAppDelegate.swift
@@ -1,8 +1,11 @@
-import ExpoModulesCore
 import GoogleSignIn
+
+#if canImport(ExpoModulesCore) 
+import ExpoModulesCore
 
 public class GoogleSignInAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     return GIDSignIn.sharedInstance.handle(url)
   }
 }
+#endif
