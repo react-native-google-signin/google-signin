dae1b26dd98186cbd5cd585562479688a7ff7d28
Vojtech Novak <vonovak@gmail.com>
2022-10-30 13:13:12 +0100

    chore: use github actions

---
+++ release.config.js
@@ -0,0 +1,26 @@
+const join = require('path').join;
+const readFileSync = require('fs').readFileSync;
+
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        writerOpts: {
+          footerPartial: readFileSync(join(__dirname, '.github/footer.hbs'), 'utf-8'),
+        },
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: 'package.json',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
