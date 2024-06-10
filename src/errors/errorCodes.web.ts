bc21ac49937b09f08c0e2366c2a29cf4a9cb027a
Vojtech Novak <vonovak@gmail.com>
2024-06-10 09:04:40 +0200

    fix: do not crash on web

---
+++ src/errors/errorCodes.web.ts
@@ -0,0 +1,7 @@
+const statusCodesRaw = {
+  SIGN_IN_CANCELLED: 'SIGN_IN_CANCELLED',
+  IN_PROGRESS: 'IN_PROGRESS',
+  PLAY_SERVICES_NOT_AVAILABLE: 'PLAY_SERVICES_NOT_AVAILABLE',
+  SIGN_IN_REQUIRED: 'SIGN_IN_REQUIRED',
+};
+export const statusCodes = Object.freeze(statusCodesRaw);
