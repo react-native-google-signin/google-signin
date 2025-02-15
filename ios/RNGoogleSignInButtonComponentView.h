0c63083445af96626d9145aec3bb962118b95348
Vojtech Novak <vonovak@gmail.com>
2025-02-15 10:27:34 +0100

    feat: improve support for RN 0.77

---
+++ ios/RNGoogleSignInButtonComponentView.h
@@ -0,0 +1,14 @@
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTViewComponentView.h>
+#import <React/RCTConversions.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNGoogleSignInButtonComponentView : RCTViewComponentView
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif
