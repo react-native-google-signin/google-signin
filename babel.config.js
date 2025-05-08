e46a1855276cecea11b2f201c4b0716e7de6ec03
Vojtech Novak <vonovak@gmail.com>
2025-05-08 20:54:34 +0200

    feat: package.json exports (#1429)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['@react-native/babel-preset'],
+  overrides: [
+    {
+      exclude: /\/node_modules\//,
+      presets: ['module:react-native-builder-bob/babel-preset'],
+    },
+    {
+      include: /\/node_modules\//,
+      presets: ['module:@react-native/babel-preset'],
+    },
+  ],
 };
